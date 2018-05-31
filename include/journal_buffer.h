0e13d2ade24380a758fe635c3e93964c45fa10d3
brad saylor <bradsayloremail@gmail.com>
2018-05-31 17:38:17 +0200

    worked on buffer functions

--- include/journal_buffer.h
+++ include/journal_buffer.h
@@ -2,5 +2,8 @@
 #define _JOURNAL_BUFFER_H_
 
 int buffer();
+int wrap_line(int ypos, int xpos, int *previous_line_length);
+int backspace_func(int ypos, int xpos, int previous_line_length);
+int nl_func(int ypos);
 
 #endif  // _JOURNAL_BUFFER_H_
--- src/journal_buffer.c
+++ src/journal_buffer.c
@@ -11,6 +11,9 @@ int buffer()
 {
     char date_stamp[display_width];
     int input_char = ' ';
+    int xpos = 0;
+    int ypos = 0;
+    int previous_line_length;
 
     get_datestamp(date_stamp);
 
@@ -21,12 +24,91 @@ int buffer()
     keypad(buffer_win, TRUE);
 
     while(1) {
+	getyx(buffer_win, ypos , xpos);
 	input_char = wgetch(buffer_win);
+	
 	if(input_char == KEY_F(1)) break;
+	else if(xpos == (display_width - 1)){
+	    wrap_line(ypos, xpos, &previous_line_length);
+	    waddch(buffer_win, input_char);
+	    wrefresh(buffer_win);
+	}
+	else if(input_char == 127) {
+	    backspace_func(ypos, xpos, previous_line_length);
+	    wrefresh(buffer_win);
+	} else if(input_char == 10) nl_func(ypos);
+	else {
 	waddch(buffer_win, input_char);
 	wrefresh(buffer_win);
+	}
+    }
+   
+    echo();
+    return 0;
+}
+
+int wrap_line(int ypos, int xpos, int *previous_line_length)
+{
+    int chars_to_space = 0;
+    int found_space_flag = 0;
+    int buffer_char = '_';
+    chtype raw_wrap_string[display_width];
+    char wrap_string[display_width];
+
+    while(!found_space_flag) {
+	buffer_char = mvwinch(buffer_win, ypos, xpos - chars_to_space) & A_CHARTEXT;
+	if(buffer_char == ' ') found_space_flag = 1;
+	chars_to_space++;
     }
 
-    echo();
+    chars_to_space--;
+
+    *previous_line_length = (xpos - chars_to_space);
+
+    wmove(buffer_win, ypos, xpos - (chars_to_space - 1));
+
+    winchnstr(buffer_win, raw_wrap_string, chars_to_space);
+    
+    for(int count = 0; count < chars_to_space - 1; count++) {
+	wrap_string[count] = raw_wrap_string[count] & A_CHARTEXT;
+	if(count == (chars_to_space - 1)) break;
+	else waddch(buffer_win, ' ');
+    }
+
+    wrap_string[chars_to_space - 1] = '\0';
+
+    mvwprintw(buffer_win, ypos + 1, 1, wrap_string);
+
+    wrefresh(buffer_win);
+
+    return 0;
+}
+
+int backspace_func(int ypos, int xpos, int previous_line_length)
+{
+    int line_position = display_width - 1;
+
+    if((ypos == 2) && (xpos == 1)) return 0;
+    else if(xpos == 1) {
+	wmove(buffer_win, ypos - 1, line_position);
+	int line_char = 0;
+	while((line_char < 33) || (line_char > 126)) {
+	    line_char =  mvwinch(buffer_win, ypos - 1, line_position) & A_CHARTEXT;
+	    line_position--;
+	    print_to_buffer(line_char, "");
+	}
+    } else {
+	wmove(buffer_win, ypos, xpos - 1);
+	waddch(buffer_win, ' ');
+	wmove(buffer_win, ypos, xpos - 1);
+    }
+    wrefresh(buffer_win);
+    return 0;
+}
+
+int nl_func(int ypos)
+{
+    wmove(buffer_win, ypos + 1, 1);
+
     return 0;
 }
